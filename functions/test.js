6082c4e0e60b1727a6a86a62e7592cae6104245c
Tom Hermans <tomhermans@gmail.com>
2020-10-03 19:36:05 +0200

    fixes 2

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,6 @@
 exports.handler = async (event, context, callback) => {
-  callback(null, { statusCode: 200, body: “ We are now split testing! ” })
-
-  return ;
-}
+	callback(null, {
+		statusCode: 200,
+		body: "We are now split testing!",
+	});
+};
